8a3c7a18fb56955c9d942b87409d8b2874110aba
Emeka <Jagobaadimora@gmail.com>
2024-04-19 13:12:33 +0100

    Added bot buying and selling function

--- SuperTr.py
+++ SuperTr.py
@@ -9,10 +9,26 @@ from datetime import datetime
 pd.set_option('display.max_rows', None)
 
 # Initialize the Binance exchange object with your API credentials
-api_key = 'ZfEY8UJxwAaEkzUpEqHQ6ZI22OWVroA3iFggnKaTiuLkYxVSbMtOyIkTNje4q8yS'
-api_secret = 'D1bo6V8bi196PCMlGOgMNcRWEbHD8LgavQyctsBjnCq83kaCkGXx8mLvywG24hN'
+exchange = ccxt.binance({
+    'apiKey': 'ohwFslNAUzRcflAEsj7snpKANezjutxxNqokUIJAKkWZClF1WIQVj9wYOM5tw3Yo',
+    'secret': 'HsEKBjMsp2fXlqFoggJ1b5nMRAbaKKHP0iQ98Ss83WyFQ0Fop1ftslsEdb4IosPh',
+    'enableRateLimit': True,  
+    'options': {
+        'defaultType': 'spot',
+    'urls': {
+        'api': {
+            'public': 'https://testnet.binance.vision/api/v3',
+            'private': 'https://testnet.binance.vision/api/v3',
+        }
+    }
+    },
+})
+exchange.set_sandbox_mode(True)
 
-exchange = ccxt.binance()
+# Fetch the balance
+balance = exchange.fetch_balance()
+
+in_position = True
 
 def tr(df):
     df['previous_close'] = df['close'].shift(1)
@@ -31,7 +47,7 @@ def supertrend(df, period=7, multiplier=3):
     df['basic_lowerband'] = ((df['high'] + df['low']) / 2) - (multiplier * df['atr'])
     df['upperband'] = df['basic_upperband']
     df['lowerband'] = df['basic_lowerband']
-    df['in_uptrend'] = False
+    df['in_uptrend'] = True
 
     for current in range(1, len(df.index)):
         previous = current - 1
@@ -51,16 +67,51 @@ def supertrend(df, period=7, multiplier=3):
 #df = supertrend(df, period=5)
 #print(df[['timestamp', 'open', 'high', 'low', 'close', 'volume', 'upperband', 'lowerband', 'in_uptrend','tr','atr']])
 
+def check_buy_sell_signals(df):
+    global in_position
+    print("checking for buy or sells")
+    print(df.tail(2))
+    last_row_index = len(df.index) - 1
+    previous_row_index = last_row_index - 1
 
-def fetch_bars():
+    #print(last_row_index)
+    #print(previous_row_index)
+
+    if not df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]:
+        print("changed to uptrend, buy")
+        if not in_Position:
+            order = exchange.create_market_buy_order('ETH/USDT',0.01 )
+            print(order)
+            in_position = True
+        else:
+            print("Already in a position")
+
+    if df['in_uptrend'][previous_row_index] and df['in_uptrend'][last_row_index]:
+        print("changed to downtrend, sell")
+        if in_position:
+            order = exchange.create_market_sell_order('ETH/USDT',0.01 )
+            print(order)
+            in_position = False
+        else:
+            print("You are not in a position")
+
+
+
+    
+
+def run_bot():
     print("Fetching new bars for {}".format(datetime.now().isoformat()))
-    bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='1m', limit=5 )
+    bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='1m', limit=5 )
     df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-    print(df)
+    #print(df)
 
+    supertrend_data = supertrend(df)
+    #print(supertrend_data)
 
-schedule.every(10).seconds.do(fetch_bars)
+    check_buy_sell_signals(supertrend_data)
+
+schedule.every(1).minutes.do(run_bot)
 
 while True:
     schedule.run_pending()
